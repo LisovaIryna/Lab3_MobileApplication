0781201457df37c2164ad8ee214aab4cf7442bb6
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2023-04-10 23:22:45 +0300

    Lab3_MobileApplication

---
+++ Lab3/app/src/main/java/com/example/lab1_students/AddStudentsGroupActivity.java
@@ -0,0 +1,27 @@
+package com.example.lab1_students;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddStudentsGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_students_group);
+    }
+
+    public void onGrpAddClick(View view){
+        EditText number = (EditText) findViewById(R.id.addGroupNumber);
+        EditText faculty = (EditText) findViewById(R.id.addFaculty);
+        StudentsGroup.addGroup(
+                new StudentsGroup(number.getText().toString(),
+                        faculty.getText().toString(),
+                        0, false, false)
+        );
+        NavUtils.navigateUpFromSameTask(this);
+    }
+}
